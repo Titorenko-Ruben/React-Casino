47d4d6397c48dad41dfcd31a9cae69effd0ce46c
kralin.front <kralin.front@gmail.com>
2022-11-30 13:17:17 +0300

    feat(FE-9): add why play banner

--- my-app/src/pages/Home/Home.jsx
+++ my-app/src/pages/Home/Home.jsx
@@ -4,12 +4,14 @@ import Discount from "shared/ui/Discount/Discount";
 import Header from "shared/ui/Header/Header";
 import SignUp from "shared/ui/SignUp/SignUp";
 import styles from './styles.module.scss'
+import PlayBanner from "shared/ui/Play-banner/Play-banner"
 
 function Home() {
   return (
     <div>
       <Header />
       <Discount />
+      <PlayBanner />
       {/* <SignUp /> */}
     </div>
   );
