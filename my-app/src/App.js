d0104db7cdf957074788959b524c352d94073295
unknown <nastya.svirid@mail.ru>
2022-12-05 14:27:00 +0300

    feat(FE-24): add sposponsorships watford

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from "pages/Home/Home";
 import SponsorEverton from "pages/SponsorEverton/SponsorEverton";
+import SponsorWatford from "pages/SponsorWatford/SponsorWatford";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/sponsorships/everton" element ={<SponsorEverton />} />
+          <Route path="/sponsorships/watford" element ={<SponsorWatford />} />
         </Routes>
       </div>
     </BrowserRouter>
