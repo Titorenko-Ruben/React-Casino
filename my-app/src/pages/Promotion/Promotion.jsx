579e10c53a1dfd80e0f7a9e6a8dd5285cf429b7b
Kiryl <bobkovum@gmail.com>
2022-12-25 23:33:28 +0300

    feat: add recent page

--- my-app/src/pages/Promotion/Promotion.jsx
+++ my-app/src/pages/Promotion/Promotion.jsx
@@ -10,7 +10,7 @@ import styles from './styles.module.scss'
 
 function Promotion() {
 	return (
-		<div classname={styles.scrollable}>
+		<div className={styles.scrollable}>
 			<div className={styles.promoWrapper}>
 				<div className={styles.titleWrapper}>
 					<div className={styles.bg}></div>
@@ -18,7 +18,11 @@ function Promotion() {
 					<div className={styles.central}>
 						<div className={styles.pageTitle}>Promotions</div>
 						<div className={styles.pageLogo}>
-							<img className={styles.imgBg} src={PromotionImg} alt='Promotion' />
+							<img
+								className={styles.imgBg}
+								src={PromotionImg}
+								alt='Promotion'
+							/>
 						</div>
 					</div>
 				</div>
@@ -28,42 +32,72 @@ function Promotion() {
 						<div className={styles.mainTitle}>Latest Promotions</div>
 						<div className={styles.itemWrapper}>
 							<div className={styles.item}>
-								<div classname={styles.img}>
-									<img className={styles.imgFoto} src={PromotionBanner1} alt='Promotion' />
+								<div className={styles.img}>
+									<img
+										className={styles.imgFoto}
+										src={PromotionBanner1}
+										alt='Promotion'
+									/>
 								</div>
-								<div className={styles.timeDescrip}>Ends at 4:00 PM 12/30/2022</div>
+								<div className={styles.timeDescrip}>
+									Ends at 4:00 PM 12/30/2022
+								</div>
 								<div className={styles.gameTitle}>Daily Races</div>
 							</div>
 
 							<div className={styles.item}>
-								<div classname={styles.img}>
-									<img className={styles.imgFoto} src={PromotionBanner2} alt='Promotion' />
+								<div className={styles.img}>
+									<img
+										className={styles.imgFoto}
+										src={PromotionBanner2}
+										alt='Promotion'
+									/>
 								</div>
-								<div className={styles.timeDescrip}>Ends at 1.59 AM 1/1/2023</div>
+								<div className={styles.timeDescrip}>
+									Ends at 1.59 AM 1/1/2023
+								</div>
 								<div className={styles.gameTitle}>Stake.us Weekly Giveway</div>
 							</div>
 
 							<div className={styles.item}>
-								<div classname={styles.img}>
-									<img className={styles.imgFoto} src={PromotionBanner3} alt='Promotion' />
+								<div className={styles.img}>
+									<img
+										className={styles.imgFoto}
+										src={PromotionBanner3}
+										alt='Promotion'
+									/>
 								</div>
-								<div className={styles.timeDescrip}>Ends at 8:00 AM 12/23/2022</div>
+								<div className={styles.timeDescrip}>
+									Ends at 8:00 AM 12/23/2022
+								</div>
 								<div className={styles.gameTitle}>Slot Battle</div>
 							</div>
 
 							<div className={styles.item}>
-								<div classname={styles.img}>
-									<img className={styles.imgFoto} src={PromotionBanner4} alt='Promotion' />
+								<div className={styles.img}>
+									<img
+										className={styles.imgFoto}
+										src={PromotionBanner4}
+										alt='Promotion'
+									/>
 								</div>
-								<div className={styles.timeDescrip}>Ends at 8:00 AM 12/28/2022</div>
+								<div className={styles.timeDescrip}>
+									Ends at 8:00 AM 12/28/2022
+								</div>
 								<div className={styles.gameTitle}>Originals Challenge</div>
 							</div>
 
 							<div className={styles.item}>
-								<div classname={styles.img}>
-									<img className={styles.imgFoto} src={PromotionBanner5} alt='Promotion' />
+								<div className={styles.img}>
+									<img
+										className={styles.imgFoto}
+										src={PromotionBanner5}
+										alt='Promotion'
+									/>
 								</div>
-								<div className={styles.timeDescrip}>Ends at 4:00 PM 12/30/2022</div>
+								<div className={styles.timeDescrip}>
+									Ends at 4:00 PM 12/30/2022
+								</div>
 								<div className={styles.gameTitle}>Challenges</div>
 							</div>
 						</div>
